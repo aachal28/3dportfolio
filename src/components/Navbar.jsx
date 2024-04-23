d4af6fcca4b8531d57508cbb05996306688ef904
aachal28 <achalpardeshi159@gmail.com>
2024-04-24 00:20:05 +0530

    added nav logo and p tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
 import React,{useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 
-import {style, styles} from '../styles';
+import {styles} from '../styles';
 import { navLinks } from '../constants';
 import { logo,menu, close } from '../assets';
 
 const Navbar = () => {
+
+const [active, SetActive]= useState("")
   return (
     <nav 
     className = {` ${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className=' w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link>
+        <Link
+        to ='/'
+        className='flex items-center gap-2'
+        onClick={()=> {
+          SetActive("");
+          window.scrollTo(0,0);
+        }}>
+          <img src ={logo} alt='logo' className='w-9 h-9 object contain' />
+          <p className=' text-white text-[18px] font-bold cursor-pointer'>Aachal 
+          <span className='sm:block hidden'> | Pardeshi </span></p>
         </Link>
+        <ul>
+          
+        </ul>
       </div>
 
     </nav>
